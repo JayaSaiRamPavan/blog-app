0d1b68efef83a3d0354caa0cc9ebe8bffa486664
750752 <tsairampavan@gmail.com>
2023-04-13 16:17:48 +0530

    2nd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { SingleCategoryComponent } from './pages/single-category/single-category.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -8,7 +11,9 @@ const routes: Routes = [
   {path : '', component : HomeComponent},
   {path : 'category', component : SingleCategoryComponent},
   {path : 'post', component : SinglePostComponent},
-
+  {path : 'about', component : AboutUsComponent},
+  {path : 'terms-conditions', component : TermsAndConditionsComponent},
+  {path : 'contact', component : ContactUsComponent}
 ];
 
 @NgModule({

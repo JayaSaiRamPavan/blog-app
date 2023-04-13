7c33d25a70f1d5c1362da65ba7d08393b4c7b117
750752 <tsairampavan@gmail.com>
2023-04-13 11:37:04 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : HomeComponent},
+  {path : 'category', component : SingleCategoryComponent},
+  {path : 'post', component : SinglePostComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
